dc89be4c49b2772d41601f48f22a3997946512e6
Vladimir Restivo <vladimir.restivo@gmail.com>
2017-03-04 19:43:47 -0800

    refactor

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/GlobalConstatnts.java
@@ -0,0 +1,16 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.Application;
+
+/**
+ * Created by devbox on 2/8/17.
+ */
+
+/**
+ * this class hold constants which can be accessed
+ * anywhere across the app, facilitating access
+ * across different modules
+ */
+public class GlobalConstatnts extends Application {
+    public static final String JOKE_TAG = "JOKE_TAG";
+}
---
+++ jokedisplay/src/main/java/com/example/jokedisplay/JokeDisplayActivity.java
@@ -0,0 +1,30 @@
+package com.example.jokedisplay;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by devbox on 2/8/17.
+ */
+
+public class JokeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+
+
+        if(intent!=null ){
+
+        }
+
+
+    }
+}
